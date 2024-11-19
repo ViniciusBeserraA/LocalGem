75fb40a30c92f339bbac2b8b968a298ce3af12a1
Vinícius Beserra <viniciusb.nitronews@gmail.com>
2024-11-19 15:26:07 -0300

    Criando mock de dados para implementacao de primeiras rotas express

--- server.js
+++ server.js
@@ -1,10 +1,55 @@
-import express from "express";
+import express, { json } from "express";
+
+const posts = [
+  {
+    descricao: "Foto de um gato",
+    imagem: "https://placecats.com/millie/300/150",
+    id: 1,
+  },
+  {
+    descricao: "Paisagem montanhosa",
+    imagem: "https://placecats.com/millie/300/150",
+    id: 2,
+  },
+  {
+    descricao: "Cachorro brincando no parque",
+    imagem: "https://placecats.com/millie/300/150",
+    id: 3,
+  },
+  {
+    descricao: "Comida deliciosa",
+    imagem: "https://placecats.com/millie/300/150",
+    id: 4,
+  },
+  {
+    descricao: "Cidade à noite",
+    imagem: "https://placecats.com/millie/300/150",
+    id: 5,
+  },
+  {
+    descricao: "Desenho animado divertido",
+    imagem: "https://placecats.com/millie/300/150",
+    id: 6,
+  },
+];
 
 const app = express();
+app.use(express.json());
+
 app.listen("3000", () => {
   console.log("servidor rodando");
 });
 
-app.get("/api", (req, res) => {
-  res.status(200).send("hello node");
+app.get("/posts", (req, res) => {
+  res.status(200).json(posts);
+});
+
+function findPostById(id) {
+  return posts.findIndex((post) => {
+    return post.id === Number(id);
+  });
+}
+
+app.get("/posts/:id", (req, res) => {
+  res.status(200).json(posts);
 });
